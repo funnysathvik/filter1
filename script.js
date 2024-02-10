2f5f0cddb2bd141584723f0f97ee607d016cf201
funnysathvik <130309025+funnysathvik@users.noreply.github.com>
2024-02-10 09:28:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,300)
+    canvas.center();
+    video = createCapture(VIDEO);
+    image(video,300,300,300,300);
+}
+
+function draw(){
+    
+}
